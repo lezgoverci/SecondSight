c1163f018a2723bf10bf1278361ccce870772d1c
vercillius <vercilliusjrmila@gmail.com>
2016-04-15 11:14:45 +0800

    added all member variables of Camera Activity Class

--- app/src/main/java/com/example/vercillius/secondsight/CameraActivity.java
+++ app/src/main/java/com/example/vercillius/secondsight/CameraActivity.java
@@ -2,11 +2,16 @@ package com.example.vercillius.secondsight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Size;
 import android.view.Window;
 import android.view.WindowManager;
 
+import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.core.Mat;
+
+import java.util.List;
 
 public class CameraActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
@@ -18,6 +23,54 @@ public class CameraActivity extends AppCompatActivity implements CameraBridgeVie
 
     // A key for storing active image size index
     private static final String STATE_IMAGE_SIZE_INDEX = "imageSizeIndex";
+
+    // ID  for items in the image size submenu
+    private static final int MENU_GROUP_ID_SIZE = 2;
+
+    // The index of the active camera
+    private int mCameraIndex;
+
+    // The index of the active image size
+    private int mImageSizeIndex;
+
+    // Is the the camera front-facing
+    private boolean mIsCameraFrontFacing;
+
+    // The number if cameras on the device
+    private int mNumCameras;
+
+    // The camera view
+    private CameraBridgeViewBase mCameraView;
+
+    // The image sizes supported by the active camera
+    private List<Size> mSupportedImageSizes;
+
+    // Whether the next camera frame should be saved as a photo
+    private boolean mIsPhotoPending;
+
+    // Matrix to save the photos
+    private Mat mBgr;
+
+    // Whether a menu action is in progress.
+    // Menu interaction should be disabled
+    private boolean mIsMenuLocked;
+
+    // The OpenCV loader callback
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(final int status) {
+            switch (status){
+                case BaseLoaderCallback.SUCCESS:
+                    Log.d(TAG,"OpenCV loaded successfully");
+                    mCameraView.enableView();
+                    mBgr = new Mat();
+                    break;
+                default:
+                    super.onManagerConnected(status);
+                    break;
+            }
+        }
+    };
 
 
     @Override
