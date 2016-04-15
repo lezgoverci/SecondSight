85b2314006f38d82168321140212544d7ef0acc9
vercillius <vercilliusjrmila@gmail.com>
2016-04-15 08:51:19 +0800

    Added Camera Activity

--- app/src/main/java/com/example/vercillius/secondsight/CameraActivity.java
+++ app/src/main/java/com/example/vercillius/secondsight/CameraActivity.java
@@ -2,12 +2,47 @@ package com.example.vercillius.secondsight;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class CameraActivity extends AppCompatActivity {
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Mat;
+
+public class CameraActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    // A tag for log output
+    private static final String TAG = CameraActivity.class.getSimpleName();
+
+    // A key for storing the active camera index
+    private static final String STATE_CAMERA_INDEX = "cameraIndex";
+
+    // A key for storing active image size index
+    private static final String STATE_IMAGE_SIZE_INDEX = "imageSizeIndex";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // keep the screen on
+        final Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
         setContentView(R.layout.activity_camera);
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return null;
     }
 }
